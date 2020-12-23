d0d10325770f516d112881f4c96bd9634caf81bb
Sungjun <opr99@naver.com>
2020-12-23 23:53:31 +0900

    #2.5 Filling Mode

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 
-ctx.strokeStyle = "##2c2c2c";
-ctx.lineWidth = 2.5;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
-canvas.width = 700;
-canvas.height = 700;
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = "INITIAL_COLOR";
+ctx.fillStyle = "INITIAL_COLOR";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 let filling = false;
@@ -36,6 +40,7 @@ function onMouseMove(event) {
 function handleColorClick(event) {
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
+  ctx.fillStyle = ctx.strokeStyle;
 }
 
 function handleRangeChange(event) {
@@ -53,11 +58,18 @@ function handleModeClick() {
   }
 }
 
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove)
   canvas.addEventListener("mousedown", startPainting)
   canvas.addEventListener("mouseup", stopPainting)
   canvas.addEventListener("mouseleave", stopPainting)
+  canvas.addEventListener("click", handleCanvasClick)
 }
 
 Array.from(colors).forEach(item =>
