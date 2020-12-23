769d470f8be37b6f994e18d5ee047851c97e83e8
Sungjun <opr99@naver.com>
2020-12-23 21:42:09 +0900

    #2.1 2D Context

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokStyle = "##2c2c2c";
+ctx.linewith = 2.5;
 
 let painting = false;
 
@@ -6,22 +10,25 @@ function stopPainting() {
   painting = false;
 }
 
-function onMouseMove(event) {
-  const x = event.offsetX;
-  const y = event.offsetY;
-}
-
-function onMouseDown(event) {
+function startPainting() {
   painting = true;
 }
 
-function onMouseUp(event) {
-  stopPainting();
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove)
-  canvas.addEventListener("mousedown", onMouseDown)
-  canvas.addEventListener("mouseup", onMouseUp)
+  canvas.addEventListener("mousedown", startPainting)
+  canvas.addEventListener("mouseup", stopPainting)
   canvas.addEventListener("mouseleave", stopPainting)
 }
